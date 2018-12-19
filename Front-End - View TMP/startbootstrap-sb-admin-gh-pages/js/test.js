7283222472ac2aaf9b6c4d3714e7301debd8dedf
Noel Pascal Schwabenland <noel@schwabenland.ch>
2018-12-19 12:43:41 +0100

    Test Files will no longer be tracked
    Deleted all Test-Files and the repo will now only track the final version

--- Front-End - View TMP/startbootstrap-sb-admin-gh-pages/js/test.js
+++
@@ -1,7 +0,0 @@
-function login(){
-    if(document.getElementById("inputPassword").value == "passwort" && document.getElementById("inputEmail").value == "admin"){
-        console.log("Success");
-        window.location.href = "index.html"
-        
-    }
-}
